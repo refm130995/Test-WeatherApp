bf09cf5b27e06c3e873afb72f6268fb89015c554
refm130995 <refm.130995@gmail.com>
2022-12-16 02:56:34 -0400

    fix(general): dockerfile

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -4,8 +4,6 @@ import { toast } from "react-toastify";
 const API_KEY = "45f18a4248ed765e86410d8b3efc75b1";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
-
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
   url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
